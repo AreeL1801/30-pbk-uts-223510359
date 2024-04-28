a3a19333a1ce40642bc2cad0a38362fab012dbcb
AreeL1801 <qushoyifarell@gmail.com>
2024-04-28 20:47:56 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+    <Kegiatan />
+  </div>
+</template>
+
+<script>
+import Kegiatan from './components/Kegiatan.vue';
+
+export default {
+  components: {
+    Kegiatan
+  }
+};
+</script>
+
+
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import './styles.css'
+
+const app = createApp(App);
+
+app.config.globalProperties.$successMessage = function (message) {
+  alert(message);
+};
+
+app.mount('#app');
